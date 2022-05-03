2798b1ade753c1dd957b489fb214c0c1422643fd
Navvaro <87947350+Navvaro@users.noreply.github.com>
2022-05-03 23:52:58 +0100

    Add files via upload

---
+++ SharedMedia_Fungistorm/Soundpaths.lua
@@ -0,0 +1,31 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cffbfff81Flurry|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Rogue\Flurry.mp3]])
+LSM:Register("sound", "|cffbfff81RollDice|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Rogue\RollDice.mp3]])
+LSM:Register("sound", "|cffbfff81Rush|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Rogue\Rush.mp3]])
+LSM:Register("sound", "|cffbfff81Vanish|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Rogue\Vanish.mp3]])
+LSM:Register("sound", "|cffbfff81Lifebloom|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Druid\Lifebloom.mp3]])
+LSM:Register("sound", "|cffbfff81TreeSoon|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Druid\Treesoon.mp3]])
+LSM:Register("sound", "|cffbfff81Efflo|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Druid\Efflo.mp3]])
+LSM:Register("sound", "|cffbfff81Swiftmend|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Druid\Swiftmend.mp3]])
+LSM:Register("sound", "|cffbfff81Flourish|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Druid\Flourish.mp3]])
+LSM:Register("sound", "|cffbfff81NesAHH|r", [[Interface\Addons\SharedMedia_Fungistorm\sound\Fungi\NesAHH.mp3]])
+
+-- -----
+--   STATUSBAR
+-- -----
